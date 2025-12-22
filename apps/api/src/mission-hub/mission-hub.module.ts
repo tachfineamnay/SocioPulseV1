63a254b9b8a03b270db39f2f55daafac9a078083
tachfineamnay <tachfine.amnay@gmail.com>
2025-12-22 21:12:29 +0100

    Fix JwtAuthGuard injection in mission hub by importing AuthModule

--- apps/api/src/mission-hub/mission-hub.module.ts
+++ apps/api/src/mission-hub/mission-hub.module.ts
@@ -4,9 +4,10 @@ import { MissionHubService } from './mission-hub.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { MissionAccessGuard } from '../common/guards';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [PrismaModule, NotificationsModule],
+    imports: [PrismaModule, NotificationsModule, AuthModule],
     controllers: [MissionHubController],
     providers: [MissionHubService, MissionAccessGuard],
     exports: [MissionHubService],
